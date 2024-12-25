e2a6aade26d43f7717959b63b147c4b02763e379
Andressa-Madureira <andressa.olmadureira@gmail.com>
2024-12-25 19:49:39 -0300

    Inclusão do campo valor,coloquei a condição e o que quero que seja mostrado

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const frm = document.querySelector("form")
 const resp1 = document.querySelector("#resp1")
 const resp2 = document.querySelector("#resp2")
+const adicionado = document.querySelector("#adicionado")
 
-frm.addEventListener("submit", (e)=>{
-    e.preventDefault()
-    const product = frm.inProduto.value
-    const quantidade = Number(frm.inQuantidade.value)
+frm.addEventListener("submit", (e) => {
+     e.preventDefault()
+     const product = frm.inProduto.value.trim().charAt(0).toUpperCase() + frm.inProduto.value.trim().slice(1).toLowerCase();
+     const quantidade = Number(frm.inQuantidade.value)
+     const valor = Number(frm.inValor.value)
 
+
+     if (product === "" || isNaN(quantidade) || quantidade <= 0) {
+          alert("Por favor, preencha os campos corretamente!");
+          return;
+     }
+     adicionado.innerHTML = `Produto adicionado com sucesso!`
+     resp1.innerHTML += `<p>Produto: ${product} - Quantidade: ${quantidade} - Valor: R$${valor.toFixed(2)}</p>`
+     resp1.style.color = "red"
     
-    if (product === "" || isNaN(quantidade) || quantidade <= 0) {
-     alert("Por favor, preencha os campos corretamente!");
-     return;
- }
+     frm.reset();
+     frm.inProduto.focus(); // Foca novamente no campo de produto
 })
